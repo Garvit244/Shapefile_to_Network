247fc7fadcd0c4fed8356e45f657b9e5f0d4af2b
Garvit244 <garvitbansal44@gmail.com>
2018-03-01 13:33:51 +0800

    Adding pagerank and dispersion metric for network

--- shapefile_to_network/main/metrics/Centrality.py
+++ shapefile_to_network/main/metrics/Centrality.py
@@ -2,10 +2,12 @@ import networkx as nx
 
 '''
     This class is used to calculate some basic metrics over the network like
+
      degree_centrality      :   number of nodes its connected to
      closeness_centrality   :   reciprocal of sum of shortest path form node u to all other nodes
      communicability        :   sum of closest walk from node u to node v
      load_centrality        :   fraction of all the shortest path passes from that node
+     nodes_pagerank         :   ranking of the nodes in the graph g on basis on incoming links
 '''
 
 
@@ -19,5 +21,15 @@ class Centrality:
         closeness_centrality = nx.closeness_centrality(self.g, distance=self.weight)
         communicability = nx.communicability(self.g)
         load_centrality = nx.load_centrality(self.g, weight=self.weight)
+        nodes_pagerank = nx.pagerank(self.g, weight=self.weight)
 
-        return degree_centrality, closeness_centrality, communicability, load_centrality
+        return degree_centrality, closeness_centrality, communicability, load_centrality, nodes_pagerank
+
+    '''
+        This function is used to find the dispersion between source and target in the network
+        @input  :   source and target node in the network
+        @output :   dictionary of nodes with dispersion score
+    '''
+
+    def nodes_dispersion(self, source, target):
+        return nx.dispersion(self.g, u=source, v=target)
