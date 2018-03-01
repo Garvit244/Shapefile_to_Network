839c6766708f27a96f17fa559eb98af54b9122ad
Garvit244 <garvitbansal44@gmail.com>
2018-03-01 11:59:33 +0800

    Adding metrics for the network

---
+++ shapefile_to_network/main/metrics/Centrality.py
@@ -0,0 +1,23 @@
+import networkx as nx
+
+'''
+    This class is used to calculate some basic metrics over the network like
+     degree_centrality      :   number of nodes its connected to
+     closeness_centrality   :   reciprocal of sum of shortest path form node u to all other nodes
+     communicability        :   sum of closest walk from node u to node v
+     load_centrality        :   fraction of all the shortest path passes from that node
+'''
+
+
+class Centrality:
+    def __init__(self, g, weight):
+        self.g = g
+        self.weight = weight
+
+    def metrics(self):
+        degree_centrality = nx.degree_centrality(self.g)
+        closeness_centrality = nx.closeness_centrality(self.g, distance=self.weight)
+        communicability = nx.communicability(self.g)
+        load_centrality = nx.load_centrality(self.g, weight=self.weight)
+
+        return degree_centrality, closeness_centrality, communicability, load_centrality
